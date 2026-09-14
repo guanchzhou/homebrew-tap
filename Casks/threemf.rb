053e6549924484152a3f097126f0016be4b33c23
Andrei Maltsev <maltsev.andrey@gmail.com>
2026-09-14 19:16:15 +0000

    Update threemf to v1.5.1

--- Casks/threemf.rb
+++ Casks/threemf.rb
@@ -1,28 +1,31 @@
 cask "threemf" do
-  version "1.5.0"
-  sha256 "25d4cbe4566640788e423f31b8a25c18b0ad6bf4afe0c888c38db28e48162fcf"
+  version "1.5.1"
+  sha256 "3a3267aaaa3f6325ebfd2f07cf723c5be5aec268b605a5b1443efc0a63b9e9be"
 
   url "https://github.com/guanchzhou/threemf/releases/download/v#{version}/threemf.zip"
   name "threemf"
   desc "Quick Look plugin for previewing .3mf, .stl, and .gcode 3D printing files"
   homepage "https://github.com/guanchzhou/threemf"
 
-  depends_on macos: :sonoma
-
-  app "threemf.app"
+  depends_on macos: ">= :sonoma"
 
   # Remove any unmanaged /Applications/threemf.app left behind by manual installs
   # (drag-and-drop from a release zip, local xcodebuild output, etc.) so the
   # subsequent install step doesn't error with "It seems there is already an App at…".
   # On a clean upgrade flow this is a no-op because brew has already uninstalled.
-  preflight_steps do
-    remove "threemf.app", base: :appdir, recursive: true
+  preflight do
+    installed = "/Applications/threemf.app"
+    if File.exist?(installed)
+      system_command "/bin/rm", args: ["-rf", installed], sudo: false
+    end
   end
 
+  app "threemf.app"
+
   zap trash: [
-    "~/Library/Containers/com.andreymaltsev.3mf-quicklook.findersync",
-    "~/Library/Containers/com.andreymaltsev.3mf-quicklook.mdimporter",
     "~/Library/Containers/com.andreymaltsev.3mf-quicklook.preview",
     "~/Library/Containers/com.andreymaltsev.3mf-quicklook.thumbnail",
+    "~/Library/Containers/com.andreymaltsev.3mf-quicklook.findersync",
+    "~/Library/Containers/com.andreymaltsev.3mf-quicklook.mdimporter",
   ]
 end
