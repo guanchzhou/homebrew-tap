3310f9ee4561fe9e3d81f1a8fec56fc95e76ad6a
Andrey Maltsev <maltsev.andrey@gmail.com>
2026-09-18 17:05:28 +0000

    Update c3s to v0.3.1

--- Formula/c3s.rb
+++ Formula/c3s.rb
@@ -1,28 +1,28 @@
 class C3s < Formula
   desc "Kubernetes TUI client"
   homepage "https://github.com/guanchzhou/c3s"
-  version "0.3.0"
+  version "0.3.1"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
       url "https://github.com/guanchzhou/c3s/releases/download/v#{version}/c3s-#{version}-darwin-arm64.tar.gz"
-      sha256 "9028e94a7da2231f709ad843c7d93693a15019b3867e433b1e61b2f093856fb2"
+      sha256 "bf45d15c12e8115d1665e465b0dddaf8c468f6e8e8d97b71beb6db1257be85e1"
     end
     on_intel do
       url "https://github.com/guanchzhou/c3s/releases/download/v#{version}/c3s-#{version}-darwin-amd64.tar.gz"
-      sha256 "69063dbfd51cf48d2a50c31d0018ba4b4393c29af1ce30b1a012bfb67ce2d1c2"
+      sha256 "6415b660fde1a0d0d25d6bcd9334295f0db072e994ff1d884a695b882df738b3"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/guanchzhou/c3s/releases/download/v#{version}/c3s-#{version}-linux-arm64.tar.gz"
-      sha256 "dff5cb5d8e468feab6f52bce787ec6e5f8ee020a67d03d3cf873cf03d8a51214"
+      sha256 "ec1ffa66fd93cd51c1fd7c6fc3b5d69685b993613f05418d46113084aa0fcbbb"
     end
     on_intel do
       url "https://github.com/guanchzhou/c3s/releases/download/v#{version}/c3s-#{version}-linux-amd64.tar.gz"
-      sha256 "7921a87c22d6ab0f6f0088465ba44f31f9c5a2f08362735b6e56833fec9c010b"
+      sha256 "d9482836905ea48963d1987db12474107fcd8ece6bee83ca92c3fc4643e87a09"
     end
   end
 
